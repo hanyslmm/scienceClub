d3ea1b6e4690b0434c4d4374480f590c7bc55e38
Hany Salama <hanyslmm>
2019-05-24 15:11:41 +0200

    read two directories

--- attendance.py
+++ attendance.py
@@ -1,33 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import numpy as np
 import pandas as pd
 import re
 import os
-listDir = os.listdir("MR.Basem Sun")
-print (os.getcwd())
-os.chdir(r"MR.Basem Sun")
-print (os.getcwd())
-for sN in listDir:
-    sheetName = sN[0:-5]
-    print (sheetName)
-    df = pd.read_excel(sheetName + ".xlsx", skip_blank_lines=False)
-    #listCol = list(df)
-    #regex = re.compile(r"Unnamed*")
-    #listUname = list(filter(regex.match, listCol))
-    # GET list of required remove unnamed columns
-    #listReq = list(set(listCol).difference(set(listUname)))
-    dfF = df.filter(regex='^(?!Unnamed).*')
-    try:
-        dfF['Name']
-        nameCount = dfF['Name'].count()
-        dfF = dfF.iloc[1:nameCount+1]
-        dfSummary = dfF.count()
-        print(nameCount)
-        print(dfSummary)
-    except:
-        print("name not found")
+os.chdir(r"test")
+mainDir = os.listdir(".")
+for subDir in mainDir:
+    print ("#############\n")
+    os.chdir(r"{}".format(subDir))
+    print (os.getcwd())
+    subDir = os.listdir(".")
+    for sN in subDir:
+        sheetName = sN[0:-5]
+        print ("#############")
+        print (sheetName)
+        print ("#############")
+        try:
+            df = pd.read_excel(sheetName + ".xlsx", skip_blank_lines=False)
+            dfF = df.filter(regex='^(?!Unnamed).*') # remove unnamed columns
+        except:
+            print("###############")
+            print (sN + "{} is not in xlsx format".format(sheetName))
+            print("###############")
+        try:
+            dfF['Name']
+            nameCount = dfF['Name'].count()
+            dfF = dfF.iloc[1:nameCount+1]
+            dfSummary = dfF.count()
+            print(dfSummary)
+        except:
+            try:
+                dfF['name']
+                nameCount = dfF['name'].count()
+                dfF = dfF.iloc[1:nameCount+1]
+                dfSummary = dfF.count()
+                print(dfSummary)
+            except:
+                print("###############")
+                print("Name not found!")
+                print("###############")
+    os.chdir("..")
 
     # engine = 'openpyxl'
     # dfSummary.to_excel(sheetName + ".xlsx", engine=engine)
