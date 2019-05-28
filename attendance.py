8ce4baf48f808f8c7798213c71c13fb0093eb374
Hany Salama <hanyslmm>
2019-05-28 23:09:48 +0200

    initial phase done

--- attendance.py
+++ attendance.py
@@ -4,46 +4,60 @@ import numpy as np
 import pandas as pd
 import re
 import os
+import xlsxwriter
+import time
+engine = 'openpyxl'
 os.chdir(r"test")
 mainDir = os.listdir(".")
+fasl = "##########################################"
+# LOOP in main directory
 for subDir in mainDir:
-    print ("#############\n")
-    os.chdir(r"{}".format(subDir))
-    print (os.getcwd())
-    subDir = os.listdir(".")
-    for sN in subDir:
-        sheetName = sN[0:-5]
-        print ("#############")
-        print (sheetName)
-        print ("#############")
-        try:
-            df = pd.read_excel(sheetName + ".xlsx", skip_blank_lines=False)
-            dfF = df.filter(regex='^(?!Unnamed).*') # remove unnamed columns
-        except:
-            print("###############")
-            print (sN + "{} is not in xlsx format".format(sheetName))
-            print("###############")
-        try:
-            dfF['Name']
-            nameCount = dfF['Name'].count()
-            dfF = dfF.iloc[1:nameCount+1]
-            dfSummary = dfF.count()
-            print(dfSummary)
-        except:
+    print ("loop in main directory of {}".format(subDir) + "\n" + fasl)
+    try:
+        time.sleep(3)
+        os.chdir(r"{}".format(subDir))
+        subDirList = os.listdir(".")
+        print (subDirList)
+# LOOP in each teacher director
+        i = 1 # start datafram name with df1
+        listDf = [] # array of DataFrame
+        listKey = [] # array of keys
+        for sN in subDirList:
+            sheetName = sN[0:-5]
+            print (fasl + "\n" +sheetName + "\n" + fasl + "\n")
             try:
-                dfF['name']
-                nameCount = dfF['name'].count()
-                dfF = dfF.iloc[1:nameCount+1]
-                dfSummary = dfF.count()
-                print(dfSummary)
+                df = pd.read_excel(sheetName + ".xlsx", skip_blank_lines=False)
+                df = df.filter(regex='^(?!Unnamed).*') # remove unnamed columns
+                try:
+                    nameCount = df['Name'].count()
+                    df = df.iloc[1:nameCount+1]
+                    # dfSummary = df.count()
+                    # print(dfSummary)
+                except:
+                    try:
+                        df['name']
+                        nameCount = df['name'].count()
+                        df = df.iloc[1:nameCount+1]
+                        # dfSummary = df.count()
+                        # print(dfSummary)
+                    except:
+                        print(fasl + "\nName column not found!\n" + fasl)
+                df = df.count().rename_axis(subDir).to_frame('counts')
+                listDf.append(df)
+                listKey.append(sheetName)
             except:
                 print("###############")
-                print("Name not found!")
+                print (sN + "{} is not in xlsx format".format(sheetName))
                 print("###############")
-    os.chdir("..")
+        newdf = pd.concat(listDf, keys = listKey)
+        newdf.to_excel(subDir + "Summary" + ".xlsx", engine=engine)
+    except:
+        print ("{} is not directory".format(subDir))
 
-    # engine = 'openpyxl'
-    # dfSummary.to_excel(sheetName + ".xlsx", engine=engine)
+
+
+# engine = 'openpyxl'
+# dfSummary.to_excel(sheetName + ".xlsx", engine=engine)
 #print (df.columns.get_loc("Aug"))
 #print (df['Sept.'].value_counts())
 #print (df['Sept.'].count())
