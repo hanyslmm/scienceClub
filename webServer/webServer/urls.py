c09ca0bf4cb449cb5f5a66f1045e3075a9453eaf
hanyslmm <hanyslmm@MacBooks-MacBook-Pro.local>
2019-10-14 06:43:36 +0200

    remove science app

--- webServer/webServer/urls.py
+++ webServer/webServer/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin, science
-from webServer import science
 from statScience.views import home_view
 from django.urls import path
 
